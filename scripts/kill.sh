331fe942b6abe520d65d69cd6fc3bfda55243dfd
raina4 <raina4@illinois.edu>
2018-02-27 03:54:19 -0600

    fixed scripts

--- scripts/kill.sh
+++ scripts/kill.sh
@@ -2,6 +2,6 @@
 
 # kill all the mxnet training processes across all hosts
 # $1 = username (raina4 or mghosh4)
-# $2 = training program python filename (train_mnist.py)
+# $2 = training program python filename including path
 python ../tools/kill-mxnet.py hosts $1 $2
 sudo python ../tools/kill-mxnet.py hosts root $2
--- scripts/run_example.sh
+++ scripts/run_example.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-# $1 = training program python filename
+# $1 = training program python filename including path
 python ../tools/launch.py -n 3 -H hosts python $1 --kv-store dist_sync
